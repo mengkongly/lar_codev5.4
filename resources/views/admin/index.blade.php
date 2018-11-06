fb7ff2320e236b98b2e7bde390a1bc6cc3b9b71b
mengkongly <mengkong2009@yahoo.com>
2018-11-06 16:18:30 -0600

    Configure admin view

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h2>Admin index</h2>
+@endsection
+
+@section('footer')
+    
+@endsection
+
---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h2>Admin User is working</h2>
+@endsection
+
+@section('footer')
+    
+@endsection
