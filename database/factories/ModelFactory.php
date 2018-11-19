92c179001028bcc263dad28c561883567f4d5ca3
mengkongly <mengkong2009@yahoo.com>
2018-11-19 15:16:17 -0600

    Added factory and seeder data

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,9 +16,72 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
+        'role_id' => $faker->numberBetween(1,3),
+        'is_active' => $faker->numberBetween(0,1),
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'remember_token' => str_random(10)
+    ];
+});
+
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'user_id' => $faker->numberBetween(1,20),
+        'category_id' => $faker->numberBetween(1,6),
+        'title' => $faker->sentence(7,20),
+        'body' => $faker->paragraph(rand(10,15), true),
+        'slug' => $faker->slug()
+    ];
+});
+
+$factory->define(App\Role::class, function (Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'name' => $faker->randomElement(['Administrator','Author','Subscriber'])
+    ];
+});
+
+$factory->define(App\Category::class, function (Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'name' => $faker->randomElement(['Electronic','Home Appliance','Outdoor Staff','Sport', 'Car/Truck','Furniture'])
+    ];
+});
+
+$factory->define(App\Photo::class, function (Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'path' => $faker->randomElement(['m_1542651506_m1.jpg','m_1542651507_m2.jpeg','post_1542426693_g4.jpg','m_1542651506_iphonex.jpg']),
+        'photoable_id' => $faker->numberBetween(1,20),
+        'photoable_type' => $faker->randomElement(['App\User','App\Post'])
+    ];
+});
+
+
+$factory->define(App\Comment::class, function (Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'post_id' => $faker->numberBetween(1,20),
+        'user_id' => $faker->numberBetween(1,10),
+        'is_active' => $faker->numberBetween(0,1),
+        'body' => $faker->sentence()
+    ];
+});
+
+$factory->define(App\CommentReply::class, function (Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'comment_id' => $faker->numberBetween(1,10),
+        'user_id' => $faker->numberBetween(1,10),
+        'is_active' => $faker->numberBetween(0,1),
+        'body' => $faker->sentence()
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,30 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table('users')->truncate();
+        DB::table('posts')->truncate();
+        DB::table('roles')->truncate();
+        DB::table('categories')->truncate();
+        DB::table('photos')->truncate();
+        DB::table('comments')->truncate();
+        DB::table('comment_replies')->truncate();
+
+
+        factory(App\Role::class,3)->create();
+        factory(App\Category::class,6)->create();
+        
+
+        factory(App\User::class,20)->create()->each(function($user){
+            $user->posts()->save(factory(App\Post::class)->make());
+        });
+
+        factory(App\Photo::class,20)->create();
+
+        factory(App\Comment::class,10)->create()->each(function($com){
+            $com->replies()->save(factory(App\CommentReply::class)->make());
+        });
+
     }
 }
