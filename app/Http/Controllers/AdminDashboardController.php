cbb7d3057fda05dfba8afaccf4cf5804ea5907cc
mengkongly <mengkong2009@yahoo.com>
2018-11-19 15:45:29 -0600

    Added simple chart to dashboard

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
+use App\Category;
+use App\Comment;
 
 class AdminDashboardController extends Controller
 {
     
     public function index(){
-        return view('admin.index');
+        $postsCount =   Post::count();
+        $categoriesCount =   Category::count();
+        $commentsCount =   Comment::count();
+
+        return view('admin.index',compact('postsCount','categoriesCount','commentsCount'));
     }
 }
